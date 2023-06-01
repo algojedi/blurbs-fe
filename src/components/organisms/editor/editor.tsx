776bcbed26e40b0aa083d4e395c732425a1f68be
Sukhdev <48499132+algojedi@users.noreply.github.com>
2023-05-31 21:14:49 -0400

    added editor styling

--- src/components/organisms/editor/editor.tsx
+++ src/components/organisms/editor/editor.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from "react"
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
 import { useParams } from "react-router-dom"
+import './editor.scss'
 
 const SAVE_INTERVAL_MS = 2000
 const TOOLBAR_OPTIONS = [
@@ -19,15 +20,23 @@ const TOOLBAR_OPTIONS = [
 export default function TextEditor() {
 	const { id: documentId } = useParams()
 	const [quill, setQuill] = useState()
-
-
-	useEffect(() => {
-		new Quill("#editor", { theme: "snow" })
-			// quill.setContents(document)
-			// quill.enable()
+	const wrapperRef = useCallback((wrapper: { innerHTML: string; append: (arg0: HTMLDivElement) => void } | null) => {
+		if (wrapper == null) return
+		wrapper.innerHTML = ""
+		const editor = document.createElement("div")
+		wrapper.append(editor)
+		// const q = 
+		new Quill(editor, {
+			theme: "snow",
+			modules: { toolbar: TOOLBAR_OPTIONS },
+		})
+		// q.disable()
+		// q.setText("Loading...")
+		// setQuill(q)
 	}, [])
+
 	// }, [quill, documentId])
-	return <div id="editor"></div>
+	return <div id="editor" className="container" ref={wrapperRef}></div>
 }
 
 
@@ -52,20 +61,4 @@ export default function TextEditor() {
 		}
 	}, [quill])
 
-	const wrapperRef = useCallback(wrapper => {
-		if (wrapper == null) return
-
-		wrapper.innerHTML = ""
-		const editor = document.createElement("div")
-		wrapper.append(editor)
-		const q = new Quill(editor, {
-			theme: "snow",
-			modules: { toolbar: TOOLBAR_OPTIONS },
-		})
-		q.disable()
-		q.setText("Loading...")
-		setQuill(q)
-	}, [])
-	return <div className="container" ref={wrapperRef}></div>
-} 
 */
