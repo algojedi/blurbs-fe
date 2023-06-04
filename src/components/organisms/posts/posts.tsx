af980e7df6fb4086cfcda818392467c3bd7bfdfd
Sukhdev <48499132+algojedi@users.noreply.github.com>
2023-06-04 16:25:16 -0400

    added dompurify. post list displays html

--- src/components/organisms/posts/posts.tsx
+++ src/components/organisms/posts/posts.tsx
@@ -5,6 +5,7 @@ import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './posts.scss';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import DOMPurify from 'dompurify';
 
 export type PostsProps = { classNames?: string; posts?: Post[] };
 
@@ -17,25 +18,33 @@ const Posts: React.FC<PostsProps> = ({ classNames, posts }) => {
       ? 'bg-dark text-white'
       : '';
 
+  const sanitizeHtml = (htmlContent: string) => {
+    const sanitizedHtml = DOMPurify.sanitize(htmlContent);
+    return sanitizedHtml;
+  };
+
   return (
     <div className={classNames}>
-      <div className="d-flex justify-content-between">
-      <h3 className='title'>Posts</h3>
-      <button className="btn-primary btn">
-            <FontAwesomeIcon
-              icon={faPlus}
-              className='mx-2'
-              // onClick={() => handleDeletePost(post?.id)}
-            />
-
-      </button>
+      <div className='d-flex justify-content-between'>
+        <h3 className='title'>Posts</h3>
+        <button className='btn-primary btn btn-sm'>
+          <FontAwesomeIcon
+            icon={faPlus}
+            className='mx-2'
+            // onClick={() => handleDeletePost(post?.id)}
+          />
+        </button>
       </div>
       {posts ? (
         posts.map((post) => (
           <NavLink to={`/posts/${post.id}`} key={post.id}>
-            <div className='card-body link-unstyled card-link'>
-              <p className='card-text'>{post.quillContent}</p>
-            </div>
+            {/* <div className='card-body link-unstyled card-link'> */}
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: sanitizeHtml(post.htmlContent),
+                }}
+              />
+            {/* </div> */}
           </NavLink>
         ))
       ) : (
